1e053a009cefc868b7400d9a8b1c20e8c201f8a9
BrownRhino <flicky_1984@hotmail.com>
2017-02-21 17:42:10 -0800

    Snake Goes Up

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-
+    print(data)
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
 
